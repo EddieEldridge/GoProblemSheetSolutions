cb88134168108b84ef0eaca1c8dcd7c90ae34456
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2017-10-05 21:43:38 +0100

    Halfway through Problem5

--- Problem5/Guessing.go
+++ Problem5/Guessing.go
@@ -5,31 +5,37 @@
 
 package main
 
-// Import 'fmt' package so we can use Printf (https://golang.org/pkg/fmt/)
+// Import 'fmt' package so we can use Printf AND Scanf(https://golang.org/pkg/fmt/)
 import "fmt"
 
-	
+// Import the math/rand function so we can generate a pseudo random number for our guessing game (https://golang.org/pkg/math/rand/)
+import "math/rand"
+
+// Import time to use as a seed for our number generator i.e every time the program is run, the time is used as a seed to generate a random number (https://golang.org/pkg/time/)
+import "time"
 
 // Main method
 func main(){
 
-	// Assign variables
-	var i uint = 1;
-	var output uint = 1;
+	// Variables
+	//var userGuess int;
 
-	// Change the value of number to choose what number 
-	// to get the factorial of
-	var number uint = 100;
-
-	// For loop to perform calculations
-	for i=1; i<=number; i++{
-		
-		output = output * i;
-
-	}
-	// Print result of calcFactorial to the screen
-	fmt.Printf("%d! =  %d",number,output)
+	seed := rand.NewSource(time.Now().UnixNano())
+	randomNumber := rand.New(seed)
 	
+	// Prompt the user for their guess
+	fmt.Printf("Please enter your guess: ");
+
+	// Code adapted from (https://stackoverflow.com/questions/14000082/how-do-i-use-fmt-scanf-in-go)
+	// incase of Windows line ending problem
+
+	
+	// Print out randomly generated number (for testing purposes)
+	fmt.Print(randomNumber)
+	// If statement to test if the user's guess was right
+	//if(userGuess==randomNumber){
+	//fmt.Printf("Congratualtions, your guess of %d was correct", userGuess);
+	//}
 }
 
 
