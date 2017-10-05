c5a41543e73883cd5bc80e78fc6875f3da331300
EddieEldridge <steadyeddie101@hotmail.co.uk>
2017-10-05 18:19:51 +0100

    My first commit

---
+++ Problem1/Problem1.go
@@ -0,0 +1,14 @@
+// Go Code written by Edward Eldridge (G00337490)
+// GMIT - Software Development
+
+package main
+
+// Import 'fmt' package so we can use Println
+import "fmt"
+
+// Main method
+func main(){
+	
+	// Print out our 'Hello' greeting using 'println' and the 'fmt' package
+	fmt.Println("Hello, 世界")
+}
