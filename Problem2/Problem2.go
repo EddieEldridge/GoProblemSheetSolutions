2b40e1eaa7bb901e464de00c59f1610b9c96dad1
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2017-10-05 18:48:51 +0100

    Test commit

--- Problem2/Problem2.go
+++ Problem2/Problem2.go
@@ -1,7 +1,6 @@
 // Go Code written by Edward Eldridge (G00337490)
 // GMIT - Software Development
 // Problem 1 taken from https://data-representation.github.io/problems/go-fundamentals.html
-// Code taken from https://tour.golang.org/welcome/1
 
 package main
 
@@ -11,8 +10,7 @@ import "fmt"
 // Main method
 func main(){
 	
-	// Print out our 'Hello' greeting to the console using 'println' and the 'fmt' package
-	fmt.Println("Hello, 世界")
+	
 }
 
 
