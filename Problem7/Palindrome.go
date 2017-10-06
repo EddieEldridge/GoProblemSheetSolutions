bcc4bf379ba94a8161b8e677bfc667d02102ba7c
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2017-10-06 17:46:41 +0100

    After Palindrome

--- Problem7/Palindrome.go
+++ Problem7/Palindrome.go
@@ -1,43 +1,35 @@
 // Go Code written by Edward Eldridge (G00337490)
 // GMIT - Software Development
 // Problem 7 taken from https://data-representation.github.io/problems/go-fundamentals.html
-
+// Code adapted from https://play.golang.org/p/5FUOzjBa-o
 
 package main
 
 // Import 'fmt' package so we can use Printf AND Scanf(https://golang.org/pkg/fmt/)
 import "fmt"
 
+// Function to check if string is a palindrome
+// returns a boolean if the inputted string is/isn't a palindrome
+func isPalindrome(input string) bool{
 
+	// Divide the string into two parts, starting from the back and front
+	// Compare them, if they are not equal to each other then return false
+	// else return true
+	for i := 0; i < len(input)/2; i++ {
+		if (input[i] != input[len(input)-i-1]){
+			return false;
+		}
+	}
+	return true;
+}
 
 // Main method
 func main(){
-
-	var i int;
-	var lowestNum int;
-	var highestNum int;
-
-	// Declare our list to loop through
-	numbers := [10]int{1,5,4,20,30,10,25,41,99,21}
 	
-	lowestNum = numbers[0];
-	highestNum = numbers[0];
-
-	// Loop through our list using range
-	for i= range numbers{
-			if(numbers[i] < lowestNum){
-				lowestNum = numbers [i];
-			}
-			if(numbers[i] > highestNum){
-				highestNum = numbers [i];
-			}
-
-	}
-
-	// Print out our highest and lowest numbers
-	fmt.Printf("Highest number: %d\n", highestNum)
-	fmt.Printf("Lowest number: %d\n", lowestNum)
-	
+	// Depending on the value put in here, a true or false value will
+	// be outputted to the console
+	// indicating if the string is a palindrome or not
+	fmt.Println(isPalindrome("anna"))
 }
 
 
